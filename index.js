b04f3717bbe1e0cf0b78c3239c486e37058db6e6
ElvisMoses <elvis.moses@student.moringaschool.com>
2024-03-19 12:08:38 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+var num1=31;
+var num2=2;
+var multiply=num1*num2;
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; 
+
+var max= Math.max(1, 12, 13, 4, 5, 20);
+
+var num3=20;
+var num4=5;
+var mod=num3/num4;
